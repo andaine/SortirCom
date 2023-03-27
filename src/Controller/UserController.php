38e0c46eb93b5ebbbcec4e37769b42151cb15ed4
andaine <andaine1@gmail.com>
2023-03-27 12:34:15 +0200

    AT : ADD user + base.twig + public/img/logo

---
+++ src/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'seConnecter')]
+    public function seConnecter(): Response
+    {
+        return $this->render('user/seConnecter.html.twig');
+    }
+}
