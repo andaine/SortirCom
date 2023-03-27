c0bd4a3bff14ea2d8971fa2510936f49bdf5cc38
Tan Latour <tan@MacBook-Pro-de-Tan.local>
2023-03-27 15:11:36 +0200

    Maj Tan 1

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    #[Route('/sorties', name: 'sorties')]
+    public function afficherSorties(SortieRepository $sortieRepository): Response
+    {
+        // todo choisir le type de tri pour l'affichage des sortie par défaut
+        $sorties = $sortieRepository->findAll();
+
+
+        return $this->render('sorties.html.twig', [
+            'controller_name' => 'SortieController',
+            "sorties"=>$sorties
+        ]);
+    }
+}
