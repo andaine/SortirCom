fe26b1e6d254ff987804652858676c40ff3d61fd
Tandelatour <tandelatour@gmail.com>
2023-03-28 11:42:43 +0200

    Maj Tan 6 affichage des sortie, ajout des entité ville et lieux

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -36,6 +36,10 @@ class Sortie
     #[ORM\JoinColumn(nullable: false)]
     private ?Etat $etat = null;
 
+    #[ORM\ManyToOne(inversedBy: 'sorties')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Lieu $lieu = null;
+
 
     public function getId(): ?int
     {
@@ -122,6 +126,18 @@ class Sortie
     public function setEtat(?Etat $etat): self
     {
         $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getLieu(): ?Lieu
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(?Lieu $lieu): self
+    {
+        $this->lieu = $lieu;
 
         return $this;
     }
