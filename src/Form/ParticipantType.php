f0b0b33dff36c5f45535bb45982119075b6c9e6c
andaine <andaine1@gmail.com>
2023-04-05 17:12:02 +0200

    AT : MODIF annuler modif user

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -7,12 +7,12 @@ use App\Entity\Site;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -27,7 +27,6 @@ class ParticipantType extends AbstractType
             ->add('nom')
             ->add('telephone')
             ->add('email')
-//            ->add('roles')
             ->add('password' , PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -90,9 +89,7 @@ class ParticipantType extends AbstractType
             ->add('enregistrer', SubmitType::class,[
                 'label' =>'Enregistrer'
             ])
-            ->add('annuler', SubmitType::class, [
-                'label' =>'Annuler'
-            ])
+
 
 //            ->add('administrateur')
 //            ->add('actif')
